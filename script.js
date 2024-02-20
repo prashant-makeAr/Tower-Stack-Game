3c12935c47229dd05601f9ac1992123de9d20b15
prashant <prashant.sharma@makear.ai>
2024-02-20 13:58:58 +0530

    Added more functionality

--- script.js
+++ script.js
@@ -1,152 +1,213 @@
 import * as THREE from "three";
 
-const canvas = document.querySelector('#canvas')
-let camera, scene , renderer 
+const canvas = document.querySelector("#canvas");
+let camera, scene, renderer;
 
-const originalBoxSize = 4
-const boxHeight = 1
-let gameStarted = false
-let stack = [] 
+const originalBoxSize = 4;
+const boxHeight = 1;
+let gameStarted = false;
+let stack = [];
+let overHangs = [];
 
-init()
+const clock = new THREE.Clock();
 
-function init () {
-    scene = new THREE.Scene() 
+init();
 
-    //Foundation
-    addLayer(0, 0, originalBoxSize, originalBoxSize) 
+function init() {
+  scene = new THREE.Scene();
 
-    //First Layer 
-    addLayer(-10, 0, originalBoxSize, originalBoxSize, 'x')
+  //Foundation
+  addLayer(0, 0, originalBoxSize, originalBoxSize);
 
-    //Set-up Lights 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6) 
-    scene.add(ambientLight) 
+  //First Layer
+  addLayer(-10, 0, originalBoxSize, originalBoxSize, "x");
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff , 0.6)
-    scene.add(directionalLight)
+  //Set-up Lights
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight);
 
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  scene.add(directionalLight);
 
-    //Camera 
-    const height = 25 
-    const aspect = window.innerWidth / window.innerHeight;
-    const width = height * (window.innerWidth /window.innerHeight) 
-    camera = new THREE.OrthographicCamera(
-        width / -2 ,
-        width / 2 ,
-        height/2 ,
-        height/-2, 
-        1, 100
-    )
+  //Camera
+  const height = 25;
+  const aspect = window.innerWidth / window.innerHeight;
+  const width = height * (window.innerWidth / window.innerHeight);
+  camera = new THREE.OrthographicCamera(
+    width / -2,
+    width / 2,
+    height / 2,
+    height / -2,
+    1,
+    100
+  );
 
-    camera.position.set(15, 15, 15)
-    camera.lookAt(0, 0, 0)
+  camera.position.set(30, 30, 30);
+  camera.lookAt(0, 0, 0);
 
-    //Renderer 
-    renderer = new THREE.WebGLRenderer({canvas : canvas, antialias : true})
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-    renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
-    // document.body.appendChild(renderer.domElement)
+  // document.body.appendChild(renderer.domElement)
 }
 
+function addLayer(x, z, width, depth, direction) {
+  const y = boxHeight + stack.length; // Add the box one layer higher
 
-function addLayer (x,  z, width, depth, direction) {
-    const y = boxHeight + stack.length // Add the box one layer higher
+  const layer = generateBox(x, y, z, width, depth);
+  layer.direction = direction;
 
-    const layer = generateBox(x, y, z, width, depth) 
-    layer.direction = direction
+  // console.log(layer);
 
-    // console.log(layer);
-
-    stack.push(layer)
+  stack.push(layer);
 }
 
+function generateBox(x, y, z, width, depth) {
+  const geometry = new THREE.BoxGeometry(width, boxHeight, depth);
 
-function generateBox (x, y, z , width, depth) {
-    const geometry = new THREE.BoxGeometry(width, boxHeight, depth)
+  const color = new THREE.Color(`hsl(${30 + stack.length * 4}, 100% , 50% )`);
+  const material = new THREE.MeshStandardMaterial({ color });
 
-    const color = new THREE.Color(`hsl(${30 + stack.length * 4}, 100% , 50% )`)
-    const material = new THREE.MeshStandardMaterial({color})
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(x, y, z);
+  scene.add(mesh);
 
-
-    const mesh = new THREE.Mesh(geometry , material)
-    mesh.position.set(x, y, z)
-    scene.add(mesh)
-
-    return {
-        threejs : mesh, 
-        width : width, 
-        depth: depth
-    }
+  return {
+    threejs: mesh,
+    width: width,
+    depth: depth,
+  };
 }
 
-function startGame () {
-    if(!gameStarted) {
-        renderer.setAnimationLoop(animation)
-        gameStarted = true
-    } else {    
-        const topLayer = stack[stack.length - 1]
-        const direction = topLayer.direction
+function startGame() {
+  if (!gameStarted) {
+    renderer.setAnimationLoop(animation);
+    gameStarted = true;
+  } else {
+    const topLayer = stack[stack.length - 1];
+    const previousLayer = stack[stack.length - 2];
 
-        //Next Layer 
-        const nextX = direction === 'x' ? 0 : -10
-        const nextZ = direction === 'z' ? 0 : - 10 
-        const newWidth = originalBoxSize 
-        const newDepth = originalBoxSize 
-        const nextDirection = direction === 'x' ? 'z' : 'x'
+    const direction = topLayer.direction;
 
-        addLayer(nextX, nextZ, newWidth, newDepth, nextDirection)
+    const delta =
+      topLayer.threejs.position[direction] -
+      previousLayer.threejs.position[direction];
+
+    const overHangSize = Math.abs(delta);
+
+    const size = direction == "x" ? topLayer.width : topLayer.depth;
+
+    const overlap = size - overHangSize;
+
+    if (overlap > 0) {
+      //Cut the layer
+      const newWidth = direction == "x" ? overlap : topLayer.width;
+      const newDepth = direction == "z" ? overlap : topLayer.depth;
+
+      //Update meta data
+      topLayer.width = newWidth;
+      topLayer.depth = newDepth;
+
+      //Update THREE.js Model
+      topLayer.threejs.scale[direction] = overlap / size;
+      topLayer.threejs.position[direction] -= delta / 2;
+
+      //Over Hanging Part
+      //   const overHangShift = (overlap / 2 + overHangSize / 2) * Math.sign(delta);
+      //   const overHangX =
+      //     direction == "x"
+      //       ? topLayer.threejs.position.x + overHangShift
+      //       : topLayer.threejs.position.x;
+
+      //   const overHangZ =
+      //     direction == "z"
+      //       ? topLayer.threejs.position.z + overHangShift
+      //       : topLayer.threejs.position.z;
+
+      //   const overHangWidth = direction == "x" ? overHangSize : newWidth;
+      //   const overHangDepth = direction == "z" ? overHangSize : newDepth;
+
+      //   addOverHang(overHangX, overHangZ, overHangWidth, overHangDepth);
+
+      //Next Layer
+      const nextX = direction === "x" ? 0 : -10;
+      const nextZ = direction === "z" ? 0 : -10;
+      const nextDirection = direction === "x" ? "z" : "x";
+      addLayer(nextX, nextZ, newWidth, newDepth, nextDirection);
     }
+
+    //Next Layer
+    // const nextX = direction === "x" ? 0 : -10;
+    // const nextZ = direction === "z" ? 0 : -10;
+    // const newWidth = originalBoxSize;
+    // const newDepth = originalBoxSize;
+    // const nextDirection = direction === "x" ? "z" : "x";
+
+    // addLayer(nextX, nextZ, newWidth, newDepth, nextDirection);
+  }
 }
 
-window.addEventListener('click' , startGame)
-window.addEventListener('keydown' , (e) => {
-    if(e.key === ' '){
-        startGame()
-    }
-})
-
-window.addEventListener('keydown', (event) => {
-    if (event.key === 'r') {
-        location.reload();
-    }
+function addOverHang(x, z, width, depth) {
+  const y = boxHeight * (stack.length - 1);
+  const overHang = generateBox(x, y, z, width, depth);
+  overHangs.push(overHang);
+}
+window.addEventListener("click", startGame);
+window.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    startGame();
+  }
 });
 
+window.addEventListener("keydown", (event) => {
+  if (event.key === "r") {
+    location.reload();
+  }
+});
 
+let time = Date.now();
 
-function animation () {
-    const speed = 0.15 
+function animation() {
+  const speed = 0.15;
 
-    const topLayer = stack[stack.length - 1] 
-    topLayer.threejs.position[topLayer.direction] += speed 
+  const deltaTime = clock.getDelta();
 
+  const topLayer = stack[stack.length - 1];
 
-    // 4 is the initial camera height 
-    if(camera.position.y < boxHeight * (stack.length - 2) + 16) {
-        camera.position.y += speed  
-    }
+  //   const amplitude = 0.1;
 
-    renderer.render(scene, camera)
+  //   const oscillation = Math.sin(Date.now() * 0.001) * amplitude;
+
+  topLayer.threejs.position[topLayer.direction] += deltaTime * speed * 50;
+
+  //   topLayer.threejs.position[topLayer.direction] =
+  //     topLayer.threejs.position[topLayer.direction] + oscillation;
+  // 4 is the initial camera height
+  if (camera.position.y < boxHeight * (stack.length - 2) + 16) {
+    camera.position.y += deltaTime * speed * 50;
+  }
+
+  renderer.render(scene, camera);
 }
 
+window.addEventListener("resize", () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-window.addEventListener('resize' , () => {
-     const width = window.innerWidth 
-     const height = window.innerHeight 
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+});
 
-    camera.updateProjectionMatrix()
-    renderer.setSize(width , height)
-})
-
-window.addEventListener('dblclick' , () => {
-    if(!document.fullscreenElement) {
-        // canvas.requestFullscreen()
-        canvas.webkitRequestFullscreen()
-    } else {
-        document.exitFullscreen()
-    }
-})
+window.addEventListener("dblclick", () => {
+  if (!document.fullscreenElement) {
+    // canvas.requestFullscreen()
+    canvas.webkitRequestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
