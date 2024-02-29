d0349ab527881c3e8c41d72ba4e8746f64fbb6a1
prashant <prashant.sharma@makear.ai>
2024-02-29 15:47:27 +0530

    updates

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ function generateBox(x, y, z, width, depth, ifFalls) {
   const geometry = new THREE.BoxGeometry(width, boxHeight, depth);
 
   const color = new THREE.Color(`hsl(${30 + stack.length * 4}, 100% , 50% )`);
-  const material = new THREE.MeshToonMaterial({ color });
+  const material = new THREE.MeshMatcapMaterial({ color });
 
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.set(x, y, z);
